b2900f9379a3ca7898ecb81005b32a560fb4babb
Min Gyo <mingyo.kim3@gmail.com>
2017-02-19 23:59:30 -0800

    update

--- listingsDB2.py
+++ listingsDB2.py
@@ -1,10 +1,13 @@
 import sqlite3
 import csv
 import urllib.request
-from webscraping import getLinks
 
 conn = sqlite3.connect('listingsALL.db')
 conn.isolation_level = None
+
+url = "http://data.insideairbnb.com/united-states/ny/new-york-city/2016-12-03/visualisations/listings.csv"
+response = urllib.request.urlopen(url)
+cr = csv.DictReader(response.read().decode('utf-8').splitlines())
 
 def create_table():
 	c.execute("CREATE TABLE IF NOT EXISTS new_york(lat REAL, long REAL, price REAL)")
@@ -12,25 +15,16 @@ def create_table():
 def data_entry(latitude, longitude, price):
 	c.execute("INSERT INTO new_york (lat, long, price) VALUES (?, ?, ?)", (latitude, longitude, price))
 
-dataLinks = getLinks();
-
-try:
+try: 
 	c = conn.cursor()
-	c.execute("DROP TABLE IF EXISTS new_york")
 	c.execute("begin")
 
+	create_table()
+	for row in cr:
+		data_entry(row['latitude'], row['longitude'], row['price'])
 
-	for link in dataLinks
-		response = urllib.request.urlopen(link)
-		cr = csv.DictReader(response.read().decode('utf-8').splitlines())
-		
-		create_table()
-		for row in cr:
-			data_entry(row['latitude'], row['longitude'], row['price'])
-		break
 	c.execute("commit")
 	c.close()
 	conn.close()
 except:
-	c.execute("rollback")
-
+	c.execute("rollback")	
--- webscraping.py
+++ webscraping.py
@@ -4,18 +4,22 @@ import urllib.request
 sauce = urllib.request.urlopen('http://insideairbnb.com/get-the-data.html').read()
 soup = bs.BeautifulSoup(sauce, 'lxml')
 
-dataLinks = []
+def getLinks():
+	dataLinks = []
 
-div = soup.find('div', class_='contentContainer')
+	div = soup.find('div', class_='contentContainer')
 
-for table in div.find_all('table'):
-    for tr in table.find('tbody').find_all('tr'):
-        rowData = tr.find_all('td')
-        if rowData[2].text == 'listings.csv':
-            data = {'City': rowData[1].text, 'Link': rowData[2].a['href']}
-            dataLinks.append(data)
-            break
-print (dataLinks)
+	for table in div.find_all('table'):
+		for tr in table.find('tbody').find_all('tr'):
+			rowData = tr.find_all('td')
+			if rowData[2].text == 'listings.csv':
+				data = {'City': rowData[1].text, 'Link': rowData[2].a['href']}
+				dataLinks.append(data)
+				break
+
+	return dataLinks
+
+#print (dataLinks)
 ##  if tr.getAttribute(2).a.text == 'listings.csv':
 ##      data = {'City': tr.getAttribute(1).text, 'Link': tr.getAttribute(2).a['href']}
 ##      linkDict.append(data)
