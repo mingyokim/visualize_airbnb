254f65e2a163037d3576111e747e713455f870cd
Nicholas Fung <nicholas.fung@alumni.ubc.ca>
2017-02-19 21:03:46 -0800

    Adding webscraping script

---
+++ webscraping.py
@@ -0,0 +1,22 @@
+import bs4 as bs
+import urllib.request
+
+sauce = urllib.request.urlopen('http://insideairbnb.com/get-the-data.html').read()
+soup = bs.BeautifulSoup(sauce, 'lxml')
+
+dataLinks = []
+
+div = soup.find('div', class_='contentContainer')
+
+for table in div.find_all('table'):
+    for tr in table.find('tbody').find_all('tr'):
+        rowData = tr.find_all('td')
+        if rowData[2].text == 'listings.csv':
+            data = {'City': rowData[1].text, 'Link': rowData[2].a['href']}
+            dataLinks.append(data)
+            break
+print (dataLinks)
+##  if tr.getAttribute(2).a.text == 'listings.csv':
+##      data = {'City': tr.getAttribute(1).text, 'Link': tr.getAttribute(2).a['href']}
+##      linkDict.append(data)
+##  print(linkDict)
