12627767e1ebc80c8320f1686d01822cbbfec5f8
Min Gyo <mingyo.kim3@gmail.com>
2017-02-20 02:18:03 -0800

    removed debug info

--- listingsDB.py
+++ listingsDB.py
@@ -14,29 +14,21 @@ def create_table(city):
 def data_entry(city, latitude, longitude, price):
 	query = "INSERT INTO '%s' (lat, long, price) VALUES (?, ?, ?)" % (city)
 	c.execute(query, (latitude, longitude, price))
-	#print(query)
 
 dataLinks = getLinks();
 print(dataLinks)
 try:
 	c = conn.cursor()
-	#print("hello")
 	c.execute("DROP TABLE IF EXISTS Amsterdam")
-	#print("drop")
 	c.execute("begin")
-	#print("begin")		
+	
 	for dataObj in dataLinks:
-		#print(dataObj)
-		#if dataObj['City'] != 'Trentino':
-		#	continue
 		try:
 			response = urllib.request.urlopen(dataObj['Link'])
 		except:
 			response = urllib.request.urlopen(urllib.parse.quote(dataObj['Link'].encode('utf-8'),':/')) 
 				
-		#print(dataObj['Link'])
 		cr = csv.DictReader(response.read().decode('utf-8').splitlines())
-	
 		create_table(dataObj['City'])
 		for row in cr:
 			data_entry(dataObj['City'], row['latitude'], row['longitude'], row['price'])
